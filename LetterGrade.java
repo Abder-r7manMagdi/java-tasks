5ce1c3ac1a7c1c9224a88e91414c330c45cd9d82
Abdelrahman magdy <abdelrahman123magdy@gmail.com>
2025-05-26 00:50:26 +0300

    Add LetterGrade.java for letter grade calculation

---
+++ LetterGrade.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LetterGrad {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Grade: ");
+        double grade = input.nextDouble();
+
+        if (grade < 50)
+            System.out.print("F");
+        else if (grade >= 50 && grade < 65)
+            System.out.print("D");
+        else if (grade >= 65 && grade < 75)
+            System.out.print("C");
+        else if (grade >= 75 && grade < 85)
+            System.out.print("B");
+        else if (grade >= 85 && grade <= 100)
+            System.out.print("A");
+        else
+            System.out.print("Invalid grade");
+    }
+}
